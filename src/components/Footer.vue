95ab59ed325b03ff647326ae61d51665387296b2
VodeniZeko <edvin.saletovic@pm.me>
2021-07-03 20:00:51 -0700

    social

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bg-gray-100">
+        <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p class="text-gray-500 text-sm text-center sm:text-left">© 2021 Copyright:
+            <a href="https://edvin.netlify.app/" class="text-gray-600 ml-1" target="_blank">Edvin Saletovic</a>
+          </p>
+          <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a href="https://www.linkedin.com/in/edvin-saletovic/" class="ml-3 text-gray-500">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+            <a href="https://github.com/VodeniZeko" class="ml-3 text-gray-500">
+              <i class="fab fa-github"></i>
+            </a>
+            <a href="mailto:salexy.es@gmail.com?subject=Hi, I am your website visitor" class="ml-3 text-gray-500">
+              <i class="fas fa-envelope-open-text"></i>
+            </a>
+            <a href="https://edvin.netlify.app/assets/Edvin_Resume.pdf" class="ml-3 text-gray-500">
+               <i class="fas fa-file"></i>
+            </a>
+          </span>
+        </div>
+      </div>
+</template>
+
+<script>
+
+export default ({
+    name:'Footer'
+})
+</script>
